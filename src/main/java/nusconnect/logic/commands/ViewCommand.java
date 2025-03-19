5815a176878f92ecd2bc0d9676ab41851cd677e7
LeeJiaWei <leejiawei828@gmail.com>
2025-03-19 21:30:49 +0800

    Add view command functionality

---
+++ src/main/java/nusconnect/logic/commands/ViewCommand.java
@@ -0,0 +1,70 @@
+package nusconnect.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import nusconnect.commons.core.index.Index;
+import nusconnect.commons.util.ToStringBuilder;
+import nusconnect.logic.Messages;
+import nusconnect.logic.commands.exceptions.CommandException;
+import nusconnect.model.Model;
+import nusconnect.model.person.Person;
+
+/**
+ * Shows the details of the specified person in the address book.
+ */
+public class ViewCommand extends Command {
+
+    public static final String COMMAND_WORD = "view";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Shows the details of the person identified by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_VIEW_PERSON_SUCCESS = "Viewing Person: %1$s";
+
+    private final Index targetIndex;
+
+    public ViewCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToView = lastShownList.get(targetIndex.getZeroBased());
+
+        return new CommandResult(String.format(MESSAGE_VIEW_PERSON_SUCCESS,
+                Messages.format(personToView)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ViewCommand)) {
+            return false;
+        }
+
+        ViewCommand otherViewCommand = (ViewCommand) other;
+        return targetIndex.equals(otherViewCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
