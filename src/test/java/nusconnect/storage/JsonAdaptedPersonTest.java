fca850c332c90ecc9993087c3d85db95c91d7017
Hackari <simreryan@gmail.com>
2025-03-24 14:26:45 +0800

    Fix checkstyle error in test

--- src/test/java/nusconnect/storage/JsonAdaptedPersonTest.java
+++ src/test/java/nusconnect/storage/JsonAdaptedPersonTest.java
@@ -79,7 +79,7 @@ public class JsonAdaptedPersonTest {
     @Test
     public void toModelType_invalidPhone_throwsIllegalValueException() {
         JsonAdaptedPerson person =
-                new JsonAdaptedPerson(VALID_NAME,  VALID_TELEGRAM,
+                new JsonAdaptedPerson(VALID_NAME, VALID_TELEGRAM,
                         INVALID_PHONE, VALID_EMAIL, VALID_ALIAS, VALID_COURSE, VALID_NOTE,
                        VALID_WEBSITE, VALID_TAGS);
         String expectedMessage = Phone.MESSAGE_CONSTRAINTS;
