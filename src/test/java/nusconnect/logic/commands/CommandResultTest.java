228021b1bc9b5aa6f5b6055d0743bca3ccf92393
Hackari <simreryan@gmail.com>
2025-03-24 19:41:57 +0800

    Modify URL in help window

--- src/test/java/nusconnect/logic/commands/CommandResultTest.java
+++ src/test/java/nusconnect/logic/commands/CommandResultTest.java
@@ -28,9 +28,6 @@ public class CommandResultTest {
         // different feedbackToUser value -> returns false
         assertFalse(commandResult.equals(new CommandResult("different")));
 
-        // different showHelp value -> returns false
-        assertFalse(commandResult.equals(new CommandResult("feedback", true, false)));
-
         // different exit value -> returns false
         assertFalse(commandResult.equals(new CommandResult("feedback", false, true)));
     }
@@ -44,9 +41,6 @@ public class CommandResultTest {
 
         // different feedbackToUser value -> returns different hashcode
         assertNotEquals(commandResult.hashCode(), new CommandResult("different").hashCode());
-
-        // different showHelp value -> returns different hashcode
-        assertNotEquals(commandResult.hashCode(), new CommandResult("feedback", true, false).hashCode());
 
         // different exit value -> returns different hashcode
         assertNotEquals(commandResult.hashCode(), new CommandResult("feedback", false, true).hashCode());
