4aad79b2d14739000aa59f4e61a6fab20561c48c
LeeJiaWei <leejiawei828@gmail.com>
2025-04-01 17:52:35 +0800

    Adjust min width and height for app

--- src/main/java/nusconnect/ui/MainWindow.java
+++ src/main/java/nusconnect/ui/MainWindow.java
@@ -149,6 +149,8 @@ public class MainWindow extends UiPart<Stage> {
     private void setWindowDefaultSize(GuiSettings guiSettings) {
         primaryStage.setHeight(guiSettings.getWindowHeight());
         primaryStage.setWidth(guiSettings.getWindowWidth());
+        primaryStage.setMinWidth(700);
+        primaryStage.setMinHeight(600);
         if (guiSettings.getWindowCoordinates() != null) {
             primaryStage.setX(guiSettings.getWindowCoordinates().getX());
             primaryStage.setY(guiSettings.getWindowCoordinates().getY());
