f20043eda3aad6c3afba873d13264e53e413251e
yjunyeu <yapjunyeu109266@gmail.com>
2025-04-02 12:55:22 +0800

    Fix grammer for sort

--- src/main/java/nusconnect/logic/commands/SortCommand.java
+++ src/main/java/nusconnect/logic/commands/SortCommand.java
@@ -12,12 +12,21 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_SUCCESS = "Sorted all persons by name in alphabetical order!";
+    public static final String MESSAGE_EMPTY_LIST = "The address book is empty!";
+    public static final String MESSAGE_ONE_PERSON = "Sorted one person!";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.sortPersonByName();
-        return new CommandResult(MESSAGE_SUCCESS);
+        int numOfPersons = model.getFilteredPersonList().size();
+        if (numOfPersons == 0) {
+            return new CommandResult(MESSAGE_EMPTY_LIST);
+        } else if (numOfPersons == 1) {
+            return new CommandResult(MESSAGE_ONE_PERSON);
+        } else {
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
     }
 }
