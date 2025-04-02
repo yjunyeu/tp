afe615c0c9fa6e1f8896418ca618179f056735f0
feconi1024 <hzactix@github.com>
2025-04-02 21:54:18 +0800

    Change module name rules

--- src/main/java/nusconnect/model/module/Module.java
+++ src/main/java/nusconnect/model/module/Module.java
@@ -9,9 +9,9 @@ import static java.util.Objects.requireNonNull;
 public class Module {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Module names should be alphanumeric, starting with 2~3 alphabets, followed by 4 numeric characters, "
-                    + "and followed by an optional character.";
-    public static final String VALIDATION_REGEX = "^[A-Za-z]{2,3}[0-9]{4}[A-Za-z]?$";
+            "Module names should be alphanumeric, starting with 2~4 alphabets, followed by 4 numeric characters, "
+                    + "and followed by one or two optional characters.";
+    public static final String VALIDATION_REGEX = "^[A-Za-z]{2,4}[0-9]{4}[A-Za-z]{0,2}$";
 
     public final String moduleName;
 
