614540bca162156abc03cf80113df8dd3721a3e7
Hackari <simreryan@gmail.com>
2025-04-04 09:38:26 +0800

    Change version number to 1.5.2

--- src/main/java/nusconnect/MainApp.java
+++ src/main/java/nusconnect/MainApp.java
@@ -36,7 +36,7 @@ import nusconnect.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 5, 0, true);
+    public static final Version VERSION = new Version(1, 5, 2, true);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
