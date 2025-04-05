e2449e9f233a937d5248a10c5c72553682195e6c
Hackari <simreryan@gmail.com>
2025-04-05 16:03:20 +0800

    Add test cases for new possible names

--- src/test/java/nusconnect/model/person/NameTest.java
+++ src/test/java/nusconnect/model/person/NameTest.java
@@ -27,15 +27,22 @@ public class NameTest {
         // invalid name
         assertFalse(Name.isValidName("")); // empty string
         assertFalse(Name.isValidName(" ")); // spaces only
-        assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
-        assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters
+        assertFalse(Name.isValidName("^")); // only invalid non-alpahnumeric
+        assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters that are not -/'.
 
         // valid name
         assertTrue(Name.isValidName("peter jack")); // alphabets only
         assertTrue(Name.isValidName("12345")); // numbers only
         assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
         assertTrue(Name.isValidName("Capital Tan")); // with capital letters
+        assertTrue(Name.isValidName("David S/O David")); // Name with /
+        assertTrue(Name.isValidName("Davidy D/O David")); // Name with /
         assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
+        assertTrue(Name.isValidName("Donald Trump Jr.")); // Name with .
+        assertTrue(Name.isValidName("Harry S. Truman")); // Name with .
+        assertTrue(Name.isValidName("Donald Trump Sr.")); // Name with .
+        assertTrue(Name.isValidName("Tan Ah-kow")); // Name with -
+        assertTrue(Name.isValidName("a 31415926 /// - / -/ ' - /'")); // technically valid name
     }
 
     @Test
