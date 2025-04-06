a9eecb5acaaf7fb017d5e77a95c69a56891e4dfd
yjunyeu <yapjunyeu109266@gmail.com>
2025-04-06 13:09:57 +0800

    Add telegram and phone checks

--- src/main/java/nusconnect/model/person/Person.java
+++ src/main/java/nusconnect/model/person/Person.java
@@ -107,7 +107,9 @@ public class Person {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName());
+                && (otherPerson.getName().equals(getName())
+                || otherPerson.getPhone().equals(getPhone())
+                || otherPerson.getTelegram().equals(getTelegram()));
     }
 
     /**
